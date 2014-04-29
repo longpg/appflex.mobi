d40ff4160339ea1e171e869bca0117fe6c44fc0c
Pham Gia Long <long@playhousedigital.com>
2014-04-29 10:40:01 +0700

    Add thumbnail to post

--- postgame.php
+++ postgame.php
@@ -28,6 +28,7 @@ if (isset($_POST['url']) && !empty($_POST['url'])):
     $xpath = new DOMXPath($dom);
     $game = array();
     $content = "";
+    $thumbnail = "";
     /**
      * Retrieve game name:
      */
@@ -82,6 +83,14 @@ if (isset($_POST['url']) && !empty($_POST['url'])):
         }
         $content = $html;
     }
+    /**
+     * Retrieve game thumbnail:
+     */
+    $results = $xpath->query("//*[@class='cover-container']/img");
+    if ($results->length > 0) {
+        $review = trim($results->item(0)->getAttribute('src'));
+        $thumbnail = $review;
+    }
 
     $post = array(
         'post_title'    => $game['name'],
@@ -109,6 +118,50 @@ if (isset($_POST['url']) && !empty($_POST['url'])):
     if (!empty($content)) {
         echo "<h2>description</h2><div>{$content}</div>";
     }
+
+    // Add Featured Image to Post
+    if (!empty($thumbnail)) {
+        $thumbnail = str_replace("https:/", "http:/", $thumbnail);
+        $upload_dir = wp_upload_dir(); // Set upload folder
+        $image_data = file_get_contents($thumbnail); // Get image data
+        $filename   = preg_replace('/[^a-z0-9]/ui', '-', $game['name']); // Create image file name
+        if (strpos($filename, ".png") === false && strpos($filename, ".jpg") === false) {
+            $filename .= ".png";
+        }
+        // Check folder permission and define file location
+        if( wp_mkdir_p( $upload_dir['path'] ) ) {
+            $file = $upload_dir['path'] . '/' . $filename;
+        } else {
+            $file = $upload_dir['basedir'] . '/' . $filename;
+        }
+        // Create the image  file on the server
+        file_put_contents( $file, $image_data );
+        // Check image file type
+        $wp_filetype = wp_check_filetype( $filename, null );
+
+        // Set attachment data
+        $attachment = array(
+            'post_mime_type' => $wp_filetype['type'],
+            'post_title'     => $game['name'],
+            'post_content'   => '',
+            'post_status'    => 'inherit'
+        );
+
+        // Create the attachment
+        $attach_id = wp_insert_attachment( $attachment, $file, $post_id );
+        // Include image.php
+        require_once('wp-admin/includes/image.php');
+
+        // Define attachment metadata
+        $attach_data = wp_generate_attachment_metadata( $attach_id, $file );
+
+        // Assign metadata to attachment
+        wp_update_attachment_metadata( $attach_id, $attach_data );
+
+        // And finally assign featured image to post
+        set_post_thumbnail( $post_id, $attach_id );
+    }
+
 endif;?>
 </body>
 </html>
